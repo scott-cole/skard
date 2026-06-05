76ba7b3d0db187507cf366e29d8d774676874ba8
Scott Cole <mcscott01@hotmail.com>
2026-06-05 13:23:38 +0100

    start screen

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,74 @@
+import { useState } from "react";
+import { drawCard } from "./game.ts";
+import type { GameState } from "./types.ts";
 import StartScreen from "./app/StartScreen";
 
 function App() {
-  return <StartScreen />;
+  const [state, setState] = useState<GameState | null>(null);
+
+  function startBattle() {
+    const initialState: GameState = {
+      phase: "player_turn",
+      hand: [],
+      deck: [
+        {
+          id: "strike_1",
+          name: "attack",
+          kind: "attack",
+          cost: 1,
+          damage: 6,
+        },
+        {
+          id: "strike_2",
+          name: "attack",
+          kind: "attack",
+          cost: 1,
+          damage: 6,
+        },
+        {
+          id: "strike_3",
+          name: "attack",
+          kind: "attack",
+          cost: 1,
+          damage: 6,
+        },
+        {
+          id: "strike_4",
+          name: "attack",
+          kind: "attack",
+          cost: 1,
+          damage: 6,
+        },
+        {
+          id: "strike_5",
+          name: "attack",
+          kind: "attack",
+          cost: 1,
+          damage: 6,
+        },
+      ],
+      discard: [],
+      energy: 3,
+      maxEnergy: 3,
+      block: 0,
+      hp: 80,
+      maxHp: 80,
+      enemies: [
+        {
+          id: 1,
+          name: "Goblin",
+          hp: 20,
+          maxHp: 20,
+          intent: "attack",
+          damage: 6,
+        },
+      ],
+    };
+    const withHand = drawCard(initialState, 5);
+    setState(withHand);
+  }
+  if (state) return <div>{JSON.stringify(state)}</div>;
+  return <StartScreen onStart={startBattle} />;
 }
 
 export default App;
--- src/app/StartScreen/index.tsx
+++ src/app/StartScreen/index.tsx
@@ -1,12 +1,15 @@
 import styles from "./StartScreen.module.scss";
 
-export default function StartScreen() {
+interface StartScreenProps {
+  onStart: () => void;
+}
+export default function StartScreen({ onStart }: StartScreenProps) {
   return (
     <div className={styles.container}>
       <h1>Skard</h1>
       <h2>Welcome to the card battle game shit</h2>
       <h3>Instructions.....</h3>
-      <button type="button" role="button">
+      <button type="button" role="button" onClick={onStart}>
         Start Game
       </button>
     </div>
